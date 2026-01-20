53d15d0057bf4436ef0b3e3548b69b0a86a16329
Vakhita <aydaevmaksim@gmail.com>
2026-01-20 20:30:08 +0300

    Поправил автотест pokemonbzttle

--- cypress/e2e/pokemonbattle.cy.js
+++ cypress/e2e/pokemonbattle.cy.js
@@ -1,17 +1,21 @@
-describe('Проверка авторизации', function () {
-/*   --- Кейс на проверку на позитивной авторизации:
-а) Ввести правильный логин
-б) Ввести правильный пароль
-в) Нажать войти
-г) Проверить нужный текст и наличие кнопки крестик --- */ 
-
-         it('Верный логин и верный пароль', function () {
-  cy.visit('https://login.qa.studio/');  // Зашли на сайт
-  cy.get('#forgotEmailButton') .should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки
-  cy.get('#mail').type('german@dolnikov.ru'); // Ввели верный логин
-  cy.get('#pass').type('qa_one_love1'); // Ввели верный пароль
-  cy.get('#loginButton').click(); // Нажал войти
-
-  cy.get('#messageHeader').should('contain.text', 'Авторизация прошла успешно').and('be.visible');
-  cy.get('#exitMessageButton > .exitIcon').should('be.visible');
-});
+describe('Проверка покупки нового аватара', function () {                 // название набора тестов
+    it('e2e тест на покупку нового аватара для тренера', function () {   // название теста
+         cy.visit('https://pokemonbattle.ru/');                          // переходим на сайт https://pokemonbattle.ru/
+         cy.get('input[id="k_email"]').type('USER_LOGIN');                   // вводим логин
+         cy.get('input[id="k_password"]').type('USER_PASSWORD');               // вводим пароль
+         cy.get('button[type="submit"]').click();                // нажимаем кнопку Подтвердить
+         cy.wait(2000);
+         cy.get('.header_card_trainer').click();            // Клик в шапке на аву тренера
+         cy.wait(2000);
+         cy.get('.k_mobile > :nth-child(5) > #dropdown > img').click(); // нажимаем кнопку Смена аватара
+         cy.get('.available > button').first().click();   // кликаем Купить у первого доступного аватара
+         cy.get('.card_number').type('4620869113632996');                     // вводим номер карты
+         cy.get('.card_csv').type('125');                             // вводим CVV карты
+         cy.get('.card_date').type('1226');                           // вводим срок действия карты
+         cy.get('.card_name').type('NAME');                           // вводим имя владельца действия карты
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();     // нажимаем кнопку Оплатить
+         cy.get('.threeds_number').type('56456');                            // вводим код подтверждения СМС
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();   // нажимаем кнопку Оплатить
+         cy.contains('Покупка прошла успешно').should('be.visible');     // проверяем наличие и видимость сообщения об успешной покупке
+     });
+ });
